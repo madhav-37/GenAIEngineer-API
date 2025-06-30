8db171af462d19d44cdd6148031cd8d8acc36e0e
madhav-37 <madhavkoli258@gmail.com>
2025-06-30 16:13:31 +0530

    first commit

---
+++ calculation.py
@@ -0,0 +1,12 @@
+import BaseCamp3
+import BaseCamp3.Day_1
+import BaseCamp3.Day_1.1_calculator
+
+print(BaseCamp3.Day_1.1_calculator.add(5, 5))
+
+
+print("hello ouskiller")
+
+a = 10
+b = 20 
+print(a + b)  
