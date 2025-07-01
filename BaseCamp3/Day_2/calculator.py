13f3c96fac6d962cdece026de04d4dcecbdd22d6
madhav-37 <madhavkoli258@gmail.com>
2025-07-01 18:50:36 +0530

    Update calculator.py
    add api_base_url for post request

--- BaseCamp3/Day_2/calculator.py
+++ BaseCamp3/Day_2/calculator.py
@@ -6,7 +6,8 @@ st.title("Calculator App")
 st.write("This app connects to a FastAPI calculator service.")
 
 # Define the API base URL
-api_url = "https://genaiengineering-cohort2-ybyi.onrender.com/"
+#api_url = "http://localhost:9321"
+api_url = "https://genaiengineer-api-0ke5.onrender.com"
 
 # Initialize session state to store the calculator display and current operation
 if 'display' not in st.session_state:
